3b7a2c1ca7ea7728e8dba5028ada8e45ab60a16c
yaso-meth <yasienmeth@gmail.com>
2024-07-26 15:19:43 +0200

    .

--- Frontend/patient_manager/lib/objects/files.dart
+++ Frontend/patient_manager/lib/objects/files.dart
@@ -2,14 +2,14 @@ class PFile {
   final int idpatient_files;
   final String file_path;
   final String file_name;
-  final int patient_id;
+  final String app_id;
   final String insert_date;
 
   const PFile(
     this.idpatient_files,
     this.file_path,
     this.file_name,
-    this.patient_id,
+    this.app_id,
     this.insert_date,
   );
 
@@ -18,8 +18,8 @@ class PFile {
       json['idpatient_files'],
       json['file_path'],
       json['file_name'],
-      json['patient_id'],
       json['insert_date'],
+      json['app_id'],
     );
   }
 }
