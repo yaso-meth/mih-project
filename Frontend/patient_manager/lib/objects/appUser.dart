e825d739548621ef21716aee4f87ac140d96f4ae
yaso-meth <yasienmeth@gmail.com>
2024-07-26 12:57:14 +0200

    renmove doc office from appUser Object

--- Frontend/patient_manager/lib/objects/appUser.dart
+++ Frontend/patient_manager/lib/objects/appUser.dart
@@ -2,7 +2,6 @@
 class AppUser {
   final int idUser;
   final String email;
-  final int docOffice_id;
   final String fname;
   final String lname;
   final String type;
@@ -12,7 +11,6 @@ class AppUser {
   const AppUser(
     this.idUser,
     this.email,
-    this.docOffice_id,
     this.fname,
     this.lname,
     this.type,
@@ -24,7 +22,6 @@ class AppUser {
     return AppUser(
       json['idUser'],
       json['email'],
-      json['docOffice_id'],
       json['fname'],
       json['lname'],
       json['type'],
