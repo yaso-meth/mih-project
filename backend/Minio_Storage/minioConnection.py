48b0d284f96a805cb568df46632029efd24b08cc
yaso-meth <yasienmeth@gmail.com>
2024-08-01 14:19:43 +0200

    prod secure change to true & version update 0.0.0.6

--- backend/Minio_Storage/minioConnection.py
+++ backend/Minio_Storage/minioConnection.py
@@ -15,5 +15,5 @@ def minioConnect(env):
         "minio.mzansi-innovation-hub.co.za",
         access_key="0RcgutfvcDq28lz7",
         secret_key="nEED72ZlKYgDqH9Iy46fVGGT9TfabGWO",
-        secure=False
+        secure=True
         )
