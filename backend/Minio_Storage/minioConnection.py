0ec80e1c21b3c4138eaa52fdcb069fcf8a37adc0
yaso-meth <yasienmeth@gmail.com>
2024-08-01 13:18:52 +0200

    remove port from minio connection

--- backend/Minio_Storage/minioConnection.py
+++ backend/Minio_Storage/minioConnection.py
@@ -3,7 +3,7 @@ from minio import Minio
 def minioConnect():
     return Minio(
     #"minio:9000",
-    "minio.mzansi-innovation-hub.co.za:9000",
+    "minio.mzansi-innovation-hub.co.za",
     access_key="0RcgutfvcDq28lz7",
     secret_key="nEED72ZlKYgDqH9Iy46fVGGT9TfabGWO",
     secure=False
