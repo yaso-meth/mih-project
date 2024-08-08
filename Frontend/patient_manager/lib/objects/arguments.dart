de8f63c116efa5de34980885bc1471fbe62f74fe
yaso-meth <yasienmeth@gmail.com>
2024-08-08 13:28:50 +0200

    update BusinessUpdateArguments to include Business Parameter

--- Frontend/patient_manager/lib/objects/arguments.dart
+++ Frontend/patient_manager/lib/objects/arguments.dart
@@ -1,16 +1,25 @@
 import 'package:patient_manager/objects/appUser.dart';
+import 'package:patient_manager/objects/business.dart';
 import 'package:patient_manager/objects/businessUser.dart';
 
 class BusinessUpdateArguments {
   final AppUser signedInUser;
   final BusinessUser? businessUser;
+  final Business? business;
 
-  BusinessUpdateArguments(this.signedInUser, this.businessUser);
+  BusinessUpdateArguments(
+    this.signedInUser,
+    this.businessUser,
+    this.business,
+  );
 }
 
 class PatientViewArguments {
   final AppUser signedInUser;
   final String type;
 
-  PatientViewArguments(this.signedInUser, this.type);
+  PatientViewArguments(
+    this.signedInUser,
+    this.type,
+  );
 }
