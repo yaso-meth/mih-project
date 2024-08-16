4c2ce26b77f48e58cf42abd64806009f16059182
yaso-meth <yasienmeth@gmail.com>
2024-08-16 11:54:20 +0200

    add revoke date and contact number to object

--- Frontend/patient_manager/lib/objects/accessRequest.dart
+++ Frontend/patient_manager/lib/objects/accessRequest.dart
@@ -4,9 +4,11 @@ class AccessRequest {
   final String app_id;
   final String date_time;
   final String access;
+  final String revoke_date;
   final String Name;
   final String type;
   final String logo_path;
+  final String contact_no;
 
   const AccessRequest({
     required this.idpatient_queue,
@@ -14,9 +16,11 @@ class AccessRequest {
     required this.app_id,
     required this.date_time,
     required this.access,
+    required this.revoke_date,
     required this.Name,
     required this.type,
     required this.logo_path,
+    required this.contact_no,
   });
 
   factory AccessRequest.fromJson(Map<String, dynamic> json) {
@@ -27,9 +31,11 @@ class AccessRequest {
         'app_id': String app_id,
         'date_time': String date_time,
         'access': String access,
+        'revoke_date': String revoke_date,
         'Name': String Name,
         'type': String type,
         'logo_path': String logo_path,
+        'contact_no': String contact_no,
       } =>
         AccessRequest(
           idpatient_queue: idpatient_queue,
@@ -37,9 +43,11 @@ class AccessRequest {
           app_id: app_id,
           date_time: date_time,
           access: access,
+          revoke_date: revoke_date,
           Name: Name,
           type: type,
           logo_path: logo_path,
+          contact_no: contact_no,
         ),
       _ => throw const FormatException('Failed to load album.'),
     };
