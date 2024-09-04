9c22125f040f8b1e09cdc67840a4533d72288e27
yaso-meth <yasienmeth@gmail.com>
2024-09-04 16:22:21 +0200

    remove usless print

--- Frontend/patient_manager/lib/pages/home.dart
+++ Frontend/patient_manager/lib/pages/home.dart
@@ -94,7 +94,7 @@ class _HomeState extends State<Home> {
             "Error: GetUserData status code ${response.statusCode}");
       }
     }
-    print(userPic);
+    //print(userPic);
     return HomeArguments(userData, bUserData, busData, userPic);
   }
 
