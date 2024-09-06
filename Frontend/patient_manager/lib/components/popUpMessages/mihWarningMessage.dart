80b1bbb16ad55c0395ac2e4abf8a06fca226f417
yaso-meth <yasienmeth@gmail.com>
2024-09-06 11:56:40 +0200

    Dynamic Pop Ups - Warnings

--- Frontend/patient_manager/lib/components/popUpMessages/mihWarningMessage.dart
+++ Frontend/patient_manager/lib/components/popUpMessages/mihWarningMessage.dart
@@ -14,16 +14,47 @@ class MIHWarningMessage extends StatefulWidget {
 
 class _MIHDeleteMessageState extends State<MIHWarningMessage> {
   var messageTypes = <String, Widget>{};
+  late double popUpWidth;
+  late double? popUpheight;
+  late double popUpTitleSize;
+  late double popUpSubtitleSize;
+  late double popUpBodySize;
+  late double popUpIconSize;
+  late double popUpPaddingSize;
   late double width;
   late double height;
+
+  void checkScreenSize() {
+    if (MzanziInnovationHub.of(context)!.theme.screenType == "desktop") {
+      setState(() {
+        popUpWidth = (width / 4) * 2;
+        popUpheight = null;
+        popUpTitleSize = 25.0;
+        popUpSubtitleSize = 20.0;
+        popUpBodySize = 15;
+        popUpPaddingSize = 25.0;
+        popUpIconSize = 100;
+      });
+    } else {
+      setState(() {
+        popUpWidth = width - (width * 0.1);
+        popUpheight = null;
+        popUpTitleSize = 20.0;
+        popUpSubtitleSize = 18.0;
+        popUpBodySize = 15;
+        popUpPaddingSize = 15.0;
+        popUpIconSize = 100;
+      });
+    }
+  }
 
   void setNoAccess() {
     messageTypes["No Access"] = Stack(
       children: [
         Container(
-          padding: const EdgeInsets.all(10.0),
-          width: 500.0,
-          height: (height / 3) * 1.5,
+          padding: EdgeInsets.all(popUpPaddingSize),
+          width: popUpWidth,
+          height: popUpheight,
           decoration: BoxDecoration(
             color: MzanziInnovationHub.of(context)!.theme.primaryColor(),
             borderRadius: BorderRadius.circular(25.0),
@@ -33,39 +64,36 @@ class _MIHDeleteMessageState extends State<MIHWarningMessage> {
           ),
           child: SingleChildScrollView(
             child: Column(
-              //mainAxisSize: MainAxisSize.max,
+              mainAxisSize: MainAxisSize.min,
               children: [
                 Icon(
                   Icons.warning_amber_rounded,
-                  size: 100,
+                  size: popUpIconSize,
                   color:
                       MzanziInnovationHub.of(context)!.theme.secondaryColor(),
                 ),
-                const SizedBox(height: 15),
+                //const SizedBox(height: 15),
                 Text(
                   "Access Pending",
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     color:
                         MzanziInnovationHub.of(context)!.theme.secondaryColor(),
-                    fontSize: 25.0,
+                    fontSize: popUpTitleSize,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
                 const SizedBox(height: 15),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Text(
-                    "Your access request is currently being reviewed.\n\nOnce approved, you'll be able to view patient data.\n\nPlease follow up with the patient to approve your access request.",
-                    style: TextStyle(
-                      color: MzanziInnovationHub.of(context)!
-                          .theme
-                          .secondaryColor(),
-                      fontSize: 15.0,
-                      fontWeight: FontWeight.bold,
-                    ),
+                Text(
+                  "Your access request is currently being reviewed.\nOnce approved, you'll be able to view patient data.\nPlease follow up with the patient to approve your access request.",
+                  style: TextStyle(
+                    color:
+                        MzanziInnovationHub.of(context)!.theme.secondaryColor(),
+                    fontSize: popUpBodySize,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
+                const SizedBox(height: 15),
               ],
             ),
           ),
@@ -94,9 +122,9 @@ class _MIHDeleteMessageState extends State<MIHWarningMessage> {
     messageTypes["Expired Access"] = Stack(
       children: [
         Container(
-          padding: const EdgeInsets.all(10.0),
-          width: 500.0,
-          height: (height / 3) * 1,
+          padding: EdgeInsets.all(popUpPaddingSize),
+          width: popUpWidth,
+          height: popUpheight,
           decoration: BoxDecoration(
             color: MzanziInnovationHub.of(context)!.theme.primaryColor(),
             borderRadius: BorderRadius.circular(25.0),
@@ -106,39 +134,36 @@ class _MIHDeleteMessageState extends State<MIHWarningMessage> {
           ),
           child: SingleChildScrollView(
             child: Column(
-              //mainAxisSize: MainAxisSize.max,
+              mainAxisSize: MainAxisSize.min,
               children: [
                 Icon(
                   Icons.warning_amber_rounded,
-                  size: 100,
+                  size: popUpIconSize,
                   color:
                       MzanziInnovationHub.of(context)!.theme.secondaryColor(),
                 ),
-                const SizedBox(height: 15),
+                //const SizedBox(height: 15),
                 Text(
                   "Access Expired",
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     color:
                         MzanziInnovationHub.of(context)!.theme.secondaryColor(),
-                    fontSize: 25.0,
+                    fontSize: popUpTitleSize,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
                 const SizedBox(height: 15),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Text(
-                    "You no longer have access to this patient profile. The authorized access period has ended. Access to a patients profile is limited to 7 days from appointment date.",
-                    style: TextStyle(
-                      color: MzanziInnovationHub.of(context)!
-                          .theme
-                          .secondaryColor(),
-                      fontSize: 15.0,
-                      fontWeight: FontWeight.bold,
-                    ),
+                Text(
+                  "You no longer have access to this patient profile. The authorized access period has ended. Access to a patients profile is limited to 7 days from appointment date.",
+                  style: TextStyle(
+                    color:
+                        MzanziInnovationHub.of(context)!.theme.secondaryColor(),
+                    fontSize: popUpBodySize,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
+                const SizedBox(height: 15),
               ],
             ),
           ),
@@ -180,6 +205,7 @@ class _MIHDeleteMessageState extends State<MIHWarningMessage> {
       width = size.width;
       height = size.height;
     });
+    checkScreenSize();
     setNoAccess();
     setExpiredAccess();
     //print(size);
