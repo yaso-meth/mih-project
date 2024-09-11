687a28f1589f51e856e28763c7f3ff2916212786
yaso-meth <yasienmeth@gmail.com>
2024-09-11 10:14:41 +0200

    browser load time improvements

--- Frontend/patient_manager/lib/pages/authentication/signin.dart
+++ Frontend/patient_manager/lib/pages/authentication/signin.dart
@@ -304,7 +304,7 @@ class _SignInState extends State<SignIn> {
                     const SizedBox(height: 10),
                     //Heading
                     Text(
-                      'Sign In',
+                      'Sign In Now',
                       style: TextStyle(
                         fontSize: 25,
                         fontWeight: FontWeight.bold,
