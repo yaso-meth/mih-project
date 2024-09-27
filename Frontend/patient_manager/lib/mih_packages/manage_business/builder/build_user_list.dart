13ac194eb39bf3ca3a2776b1919177344a1faf26
yaso <yasienmeth@gmail.com>
2024-09-27 12:05:41 +0200

    user search window enhancement

--- Frontend/patient_manager/lib/mih_packages/manage_business/builder/build_user_list.dart
+++ Frontend/patient_manager/lib/mih_packages/manage_business/builder/build_user_list.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:patient_manager/mih_components/mih_inputs_and_buttons/mih_button.dart';
 import 'package:patient_manager/mih_components/mih_inputs_and_buttons/mih_dropdown_input.dart';
 import 'package:patient_manager/mih_components/mih_inputs_and_buttons/mih_text_input.dart';
+import 'package:patient_manager/mih_components/mih_layout/mih_window.dart';
 import 'package:patient_manager/mih_components/mih_pop_up_messages/mih_error_message.dart';
 import 'package:patient_manager/mih_components/mih_pop_up_messages/mih_loading_circle.dart';
 import 'package:patient_manager/mih_components/mih_pop_up_messages/mih_success_message.dart';
@@ -121,120 +122,188 @@ class _BuildUserListState extends State<BuildUserList> {
       lnameController.text = hideEmail(widget.users[index].email);
     });
     showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (context) => Dialog(
-        child: Stack(
-          children: [
-            Container(
-              padding: const EdgeInsets.all(10.0),
-              width: 700.0,
-              //height: 475.0,
-              decoration: BoxDecoration(
-                color: MzanziInnovationHub.of(context)!.theme.primaryColor(),
-                borderRadius: BorderRadius.circular(25.0),
-                border: Border.all(
-                    color:
-                        MzanziInnovationHub.of(context)!.theme.secondaryColor(),
-                    width: 5.0),
+        context: context,
+        barrierDismissible: false,
+        builder: (context) => MIHWindow(
+            fullscreen: false,
+            windowTitle: "Add Employee",
+            windowBody: [
+              const SizedBox(height: 10.0),
+              MIHTextField(
+                controller: fnameController,
+                hintText: "Username Name",
+                editable: false,
+                required: true,
               ),
-              child: SingleChildScrollView(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      "Add Employee",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: MzanziInnovationHub.of(context)!
-                            .theme
-                            .secondaryColor(),
-                        fontSize: 35.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 25.0),
-                    MIHTextField(
-                      controller: fnameController,
-                      hintText: "Username Name",
-                      editable: false,
-                      required: true,
-                    ),
-                    const SizedBox(height: 10.0),
-                    MIHTextField(
-                      controller: lnameController,
-                      hintText: "Email",
-                      editable: false,
-                      required: true,
-                    ),
-                    const SizedBox(height: 10.0),
-                    MIHDropdownField(
-                      controller: typeController,
-                      hintText: "Title",
-                      dropdownOptions: const ["Doctor", "Assistant"],
-                      required: true,
-                      editable: true,
-                    ),
-                    const SizedBox(height: 10.0),
-                    MIHDropdownField(
-                      controller: accessController,
-                      hintText: "Access",
-                      dropdownOptions: const ["Full", "Partial"],
-                      required: true,
-                      editable: true,
-                    ),
-                    const SizedBox(height: 30.0),
-                    SizedBox(
-                      width: 300,
-                      height: 50,
-                      child: MIHButton(
-                        buttonText: "Add",
-                        buttonColor: MzanziInnovationHub.of(context)!
-                            .theme
-                            .secondaryColor(),
-                        textColor: MzanziInnovationHub.of(context)!
-                            .theme
-                            .primaryColor(),
-                        onTap: () {
-                          if (isRequiredFieldsCaptured()) {
-                            createBusinessUserAPICall(index);
-                          } else {
-                            showDialog(
-                              context: context,
-                              builder: (context) {
-                                return const MIHErrorMessage(
-                                    errorType: "Input Error");
-                              },
-                            );
-                          }
+              const SizedBox(height: 10.0),
+              MIHTextField(
+                controller: lnameController,
+                hintText: "Email",
+                editable: false,
+                required: true,
+              ),
+              const SizedBox(height: 10.0),
+              MIHDropdownField(
+                controller: typeController,
+                hintText: "Title",
+                dropdownOptions: const ["Doctor", "Assistant"],
+                required: true,
+                editable: true,
+              ),
+              const SizedBox(height: 10.0),
+              MIHDropdownField(
+                controller: accessController,
+                hintText: "Access",
+                dropdownOptions: const ["Full", "Partial"],
+                required: true,
+                editable: true,
+              ),
+              const SizedBox(height: 15.0),
+              SizedBox(
+                width: 300,
+                height: 50,
+                child: MIHButton(
+                  buttonText: "Add",
+                  buttonColor:
+                      MzanziInnovationHub.of(context)!.theme.secondaryColor(),
+                  textColor:
+                      MzanziInnovationHub.of(context)!.theme.primaryColor(),
+                  onTap: () {
+                    if (isRequiredFieldsCaptured()) {
+                      createBusinessUserAPICall(index);
+                    } else {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return const MIHErrorMessage(
+                              errorType: "Input Error");
                         },
-                      ),
-                    )
-                  ],
+                      );
+                    }
+                  },
                 ),
               ),
-            ),
-            Positioned(
-              top: 5,
-              right: 5,
-              width: 50,
-              height: 50,
-              child: IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                icon: Icon(
-                  Icons.close,
-                  color: MzanziInnovationHub.of(context)!.theme.errorColor(),
-                  size: 35,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+              const SizedBox(height: 10.0),
+            ],
+            windowTools: [],
+            onWindowTapClose: () {
+              Navigator.pop(context);
+            }));
+    // showDialog(
+    //   context: context,
+    //   barrierDismissible: false,
+    //   builder: (context) => Dialog(
+    //     child: Stack(
+    //       children: [
+    //         Container(
+    //           padding: const EdgeInsets.all(10.0),
+    //           width: 700.0,
+    //           //height: 475.0,
+    //           decoration: BoxDecoration(
+    //             color: MzanziInnovationHub.of(context)!.theme.primaryColor(),
+    //             borderRadius: BorderRadius.circular(25.0),
+    //             border: Border.all(
+    //                 color:
+    //                     MzanziInnovationHub.of(context)!.theme.secondaryColor(),
+    //                 width: 5.0),
+    //           ),
+    //           child: SingleChildScrollView(
+    //             padding: const EdgeInsets.symmetric(horizontal: 10),
+    //             child: Column(
+    //               mainAxisSize: MainAxisSize.min,
+    //               children: [
+    //                 Text(
+    //                   "Add Employee",
+    //                   textAlign: TextAlign.center,
+    //                   style: TextStyle(
+    //                     color: MzanziInnovationHub.of(context)!
+    //                         .theme
+    //                         .secondaryColor(),
+    //                     fontSize: 35.0,
+    //                     fontWeight: FontWeight.bold,
+    //                   ),
+    //                 ),
+    //                 const SizedBox(height: 25.0),
+    //                 MIHTextField(
+    //                   controller: fnameController,
+    //                   hintText: "Username Name",
+    //                   editable: false,
+    //                   required: true,
+    //                 ),
+    //                 const SizedBox(height: 10.0),
+    //                 MIHTextField(
+    //                   controller: lnameController,
+    //                   hintText: "Email",
+    //                   editable: false,
+    //                   required: true,
+    //                 ),
+    //                 const SizedBox(height: 10.0),
+    //                 MIHDropdownField(
+    //                   controller: typeController,
+    //                   hintText: "Title",
+    //                   dropdownOptions: const ["Doctor", "Assistant"],
+    //                   required: true,
+    //                   editable: true,
+    //                 ),
+    //                 const SizedBox(height: 10.0),
+    //                 MIHDropdownField(
+    //                   controller: accessController,
+    //                   hintText: "Access",
+    //                   dropdownOptions: const ["Full", "Partial"],
+    //                   required: true,
+    //                   editable: true,
+    //                 ),
+    //                 const SizedBox(height: 30.0),
+    //                 SizedBox(
+    //                   width: 300,
+    //                   height: 50,
+    //                   child: MIHButton(
+    //                     buttonText: "Add",
+    //                     buttonColor: MzanziInnovationHub.of(context)!
+    //                         .theme
+    //                         .secondaryColor(),
+    //                     textColor: MzanziInnovationHub.of(context)!
+    //                         .theme
+    //                         .primaryColor(),
+    //                     onTap: () {
+    //                       if (isRequiredFieldsCaptured()) {
+    //                         createBusinessUserAPICall(index);
+    //                       } else {
+    //                         showDialog(
+    //                           context: context,
+    //                           builder: (context) {
+    //                             return const MIHErrorMessage(
+    //                                 errorType: "Input Error");
+    //                           },
+    //                         );
+    //                       }
+    //                     },
+    //                   ),
+    //                 )
+    //               ],
+    //             ),
+    //           ),
+    //         ),
+    //         Positioned(
+    //           top: 5,
+    //           right: 5,
+    //           width: 50,
+    //           height: 50,
+    //           child: IconButton(
+    //             onPressed: () {
+    //               Navigator.pop(context);
+    //             },
+    //             icon: Icon(
+    //               Icons.close,
+    //               color: MzanziInnovationHub.of(context)!.theme.errorColor(),
+    //               size: 35,
+    //             ),
+    //           ),
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    // );
   }
 
   @override
