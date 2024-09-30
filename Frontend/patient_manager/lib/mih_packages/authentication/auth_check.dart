ac465db055bc9db97b0dc586eeeeacc15e93d045
yaso <yasienmeth@gmail.com>
2024-09-30 09:01:12 +0200

    auth flow update

--- Frontend/patient_manager/lib/mih_packages/authentication/auth_check.dart
+++ Frontend/patient_manager/lib/mih_packages/authentication/auth_check.dart
@@ -3,18 +3,31 @@ import 'package:patient_manager/mih_packages/authentication/signin_or_register.d
 import 'package:patient_manager/mih_packages/mih_home/home.dart';
 import 'package:supertokens_flutter/supertokens.dart';
 
-class AuthCheck extends StatelessWidget {
+class AuthCheck extends StatefulWidget {
   const AuthCheck({super.key});
+
+  @override
+  State<AuthCheck> createState() => _AuthCheckState();
+}
+
+class _AuthCheckState extends State<AuthCheck> {
+  late Future<bool> signedIn;
 
   Future<bool> doesSessionExist() async {
     return await SuperTokens.doesSessionExist();
   }
 
   @override
+  void initState() {
+    signedIn = doesSessionExist();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: FutureBuilder(
-          future: doesSessionExist(),
+          future: signedIn,
           builder: (context, snapshot) {
             if (snapshot.data == true) {
               return const Home();
