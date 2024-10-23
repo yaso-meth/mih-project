f4314cca2c6272e46d054a21438b5fa1e8d97223
yaso <yasienmeth@gmail.com>
2024-10-23 12:15:53 +0200

    Add geolocate plugin

--- Frontend/patient_manager/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Frontend/patient_manager/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import app_links
 import device_info_plus
+import geolocator_apple
 import google_sign_in_ios
 import no_screenshot
 import path_provider_foundation
@@ -19,6 +20,7 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   NoScreenshotPlugin.register(with: registry.registrar(forPlugin: "NoScreenshotPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
--- Frontend/patient_manager/windows/flutter/generated_plugins.cmake
+++ Frontend/patient_manager/windows/flutter/generated_plugins.cmake
@@ -4,6 +4,7 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   app_links
+  geolocator_windows
   printing
   syncfusion_pdfviewer_windows
   url_launcher_windows
