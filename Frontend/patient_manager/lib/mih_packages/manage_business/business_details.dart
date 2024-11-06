1e178bbb368c38771a942b597682a9bc48ea50c0
yaso <yasienmeth@gmail.com>
2024-11-06 09:11:04 +0200

    email validation fix

--- Frontend/patient_manager/lib/mih_packages/manage_business/business_details.dart
+++ Frontend/patient_manager/lib/mih_packages/manage_business/business_details.dart
@@ -262,7 +262,7 @@ class _BusinessDetailsState extends State<BusinessDetails> {
 
   bool validEmail() {
     String text = emailController.text;
-    var regex = RegExp(r'^[a-zA-Z0-9]+@[a-zA-Z.-]+\.[a-zA-Z]{2,}$');
+    var regex = RegExp(r'^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$');
     return regex.hasMatch(text);
   }
 
