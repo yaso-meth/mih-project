9ffd63847a5bf0327683bdd6e80f718847bdc1bd
yaso <yasienmeth@gmail.com>
2024-11-19 09:39:59 +0200

    .

--- Frontend/lib/mih_env/env.dart
+++ Frontend/lib/mih_env/env.dart
@@ -4,6 +4,7 @@ enum Enviroment { dev, prod }
 abstract class AppEnviroment {
   static late String baseApiUrl;
   static late String baseFileUrl;
+  static late String whatsappAccessToken;
   static late String fingerPrintPluginKey;
   static late Enviroment _enviroment;
   static Enviroment get enviroment => _enviroment;
@@ -14,6 +15,8 @@ abstract class AppEnviroment {
         {
           baseApiUrl = "http://localhost:8080";
           baseFileUrl = "http://localhost:9000";
+          whatsappAccessToken =
+              "EAAPINXuNFdYBOzBjTcvZA2iPXEHbHRF9uNXyP3ihkPRUcBqKNru5g9NKRRKkFaiaITEzO3BMo6CjdUmlDH4qYTW2mzDrZB4Q21ZCEZBgECZCu27vfaOXJZCYQLNxwoXkrZBRYv8ZAP37f69r3z9JxLQxdxn9gwqA3oNZAlBBRapJQzxOr6pZBTdI3bbjbu17ZBIwRcF4JCqPDCNLEZCI3bmHwEd2i2niNMYZD";
           //fingerPrintPluginKey = 'h5X7a5j14iUZCobI1ZeX';
           break;
         }
