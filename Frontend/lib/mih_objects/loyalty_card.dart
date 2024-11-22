f5b1a4328d1c547064757079f56611940b2c08cb
yaso <yasienmeth@gmail.com>
2024-11-22 12:00:16 +0200

    update object name to MIHLoyaltyCard

--- Frontend/lib/mih_objects/loyalty_card.dart
+++ Frontend/lib/mih_objects/loyalty_card.dart
@@ -1,17 +1,17 @@
-class MIHNotification {
+class MIHLoyaltyCard {
   final int idloyalty_cards;
   final String app_id;
   final String shop_name;
   final String card_number;
 
-  const MIHNotification({
+  const MIHLoyaltyCard({
     required this.idloyalty_cards,
     required this.app_id,
     required this.shop_name,
     required this.card_number,
   });
 
-  factory MIHNotification.fromJson(Map<String, dynamic> json) {
+  factory MIHLoyaltyCard.fromJson(Map<String, dynamic> json) {
     return switch (json) {
       {
         "idloyalty_cards": int idloyalty_cards,
@@ -19,7 +19,7 @@ class MIHNotification {
         "shop_name": String shop_name,
         "notification_read": String card_number,
       } =>
-        MIHNotification(
+        MIHLoyaltyCard(
           idloyalty_cards: idloyalty_cards,
           app_id: app_id,
           shop_name: shop_name,
