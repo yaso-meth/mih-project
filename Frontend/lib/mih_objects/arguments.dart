ecdde2d5e2e54b0bcab53d44174733b7bde7ff96
yaso <yasienmeth@gmail.com>
2024-12-04 13:56:17 +0200

    add ClaimStatementGenerationArguments to be used when calling generate claim statement

--- Frontend/lib/mih_objects/arguments.dart
+++ Frontend/lib/mih_objects/arguments.dart
@@ -99,3 +99,61 @@ class PatientEditArguments {
     this.selectedPatient,
   );
 }
+
+class ClaimStatementGenerationArguments {
+  final String document_type;
+  final String patient_app_id;
+  final String patient_full_name;
+  final String patient_id_no;
+  final String has_med_aid;
+  final String med_aid_no;
+  final String med_aid_code;
+  final String med_aid_name;
+  final String med_aid_scheme;
+  final String busName;
+  final String busAddr;
+  final String busNo;
+  final String busEmail;
+  final String provider_name;
+  final String practice_no;
+  final String vat_no;
+  final String service_date;
+  final String service_desc;
+  final String service_desc_option;
+  final String procedure_name;
+  final String procedure_additional_info;
+  final String icd10_code;
+  final String amount;
+  final String pre_auth_no;
+  final String logo_path;
+  final String sig_path;
+
+  ClaimStatementGenerationArguments(
+    this.document_type,
+    this.patient_app_id,
+    this.patient_full_name,
+    this.patient_id_no,
+    this.has_med_aid,
+    this.med_aid_no,
+    this.med_aid_code,
+    this.med_aid_name,
+    this.med_aid_scheme,
+    this.busName,
+    this.busAddr,
+    this.busNo,
+    this.busEmail,
+    this.provider_name,
+    this.practice_no,
+    this.vat_no,
+    this.service_date,
+    this.service_desc,
+    this.service_desc_option,
+    this.procedure_name,
+    this.procedure_additional_info,
+    this.icd10_code,
+    this.amount,
+    this.pre_auth_no,
+    this.logo_path,
+    this.sig_path,
+  );
+}
