88310af7d4d427e097651dc0562dd59ab9259b46
yaso <yasienmeth@gmail.com>
2025-01-24 12:15:59 +0200

    add ai package

---
+++ Frontend/lib/mih_packages/mzansi_ai/mzansi_ai.dart
@@ -0,0 +1,78 @@
+import 'package:Mzansi_Innovation_Hub/mih_components/mih_package/mih_app.dart';
+import 'package:Mzansi_Innovation_Hub/mih_components/mih_package/mih_app_action.dart';
+import 'package:Mzansi_Innovation_Hub/mih_components/mih_package/mih_app_tools.dart';
+import 'package:Mzansi_Innovation_Hub/mih_objects/app_user.dart';
+import 'package:Mzansi_Innovation_Hub/mih_objects/arguments.dart';
+import 'package:Mzansi_Innovation_Hub/mih_packages/mzansi_ai/ai_chat.dart';
+import 'package:flutter/material.dart';
+
+class MzansiAi extends StatefulWidget {
+  final AppUser signedInUser;
+  const MzansiAi({
+    super.key,
+    required this.signedInUser,
+  });
+
+  @override
+  State<MzansiAi> createState() => _MzansiAiState();
+}
+
+class _MzansiAiState extends State<MzansiAi> {
+  int _selcetedIndex = 0;
+
+  MihAppAction getAction() {
+    return MihAppAction(
+      icon: const Icon(Icons.arrow_back),
+      iconSize: 35,
+      onTap: () {
+        Navigator.of(context).pop();
+        Navigator.of(context).popAndPushNamed(
+          '/',
+          arguments: AuthArguments(true, false),
+        );
+      },
+    );
+  }
+
+  MihAppTools getTools() {
+    Map<Widget, void Function()?> temp = {};
+    temp[const Icon(Icons.chat)] = () {
+      setState(() {
+        _selcetedIndex = 0;
+      });
+    };
+
+    return MihAppTools(
+      tools: temp,
+      selcetedIndex: _selcetedIndex,
+    );
+  }
+
+  List<Widget> getToolBody() {
+    List<Widget> toolBodies = [
+      AiChat(signedInUser: widget.signedInUser),
+    ];
+    return toolBodies;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MihApp(
+      appActionButton: getAction(),
+      appTools: getTools(),
+      appBody: getToolBody(),
+      selectedbodyIndex: _selcetedIndex,
+      onIndexChange: (newValue) {
+        setState(() {
+          _selcetedIndex = newValue;
+        });
+        print("Index: $_selcetedIndex");
+      },
+    );
+  }
+}
