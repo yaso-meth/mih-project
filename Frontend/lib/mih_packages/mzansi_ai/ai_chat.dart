9803a4c6c3faf84a51dbe9e084fb56dd6acbf815
yaso <yasienmeth@gmail.com>
2025-01-25 13:49:04 +0200

    .

--- Frontend/lib/mih_packages/mzansi_ai/ai_chat.dart
+++ Frontend/lib/mih_packages/mzansi_ai/ai_chat.dart
@@ -33,9 +33,6 @@ class _AiChatState extends State<AiChat> {
       "You are a helpful and friendly AI assistant. You are running on a system called MIH which was created by \"Mzansi Innovation Hub\" a South African based company.";
   final client = ollama.OllamaClient(
     baseUrl: "${AppEnviroment.baseAiUrl}/api",
-    headers: {
-      "Content-Type": "application/json",
-    },
   );
   List<ollama.Message> _chatHistory = [];
 
