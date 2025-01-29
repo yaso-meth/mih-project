3f45c441cbbe6fd848a2f6f07eb993d3dae8e135
yaso <yasienmeth@gmail.com>
2025-01-29 09:59:29 +0200

    remove theme setting from main to auth check

--- Frontend/lib/main.dart
+++ Frontend/lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
-//import 'package:fpjs_pro_plugin/error.dart';
 import '../mih_env/env.dart';
 import '../mih_router/routeGenerator.dart';
 import '../mih_theme/mih_theme.dart';
-
-//import 'package:fpjs_pro_plugin/fpjs_pro_plugin.dart';
 
 class MzanziInnovationHub extends StatefulWidget {
   const MzanziInnovationHub({
@@ -99,14 +95,14 @@ class _MzanziInnovationHubState extends State<MzanziInnovationHub> {
 
   @override
   Widget build(BuildContext context) {
-    var brightness =
-        SchedulerBinding.instance.platformDispatcher.platformBrightness;
-    bool isDarkMode = brightness == Brightness.dark;
-    if (isDarkMode) {
-      theme.mode = "Dark";
-    } else {
-      theme.mode = "Light";
-    }
+    // var brightness =
+    //     SchedulerBinding.instance.platformDispatcher.platformBrightness;
+    // bool isDarkMode = brightness == Brightness.dark;
+    // if (isDarkMode) {
+    //   theme.mode = "Dark";
+    // } else {
+    //   theme.mode = "Light";
+    // }
     double width = MediaQuery.sizeOf(context).width;
     theme.setScreenType(width);
     precacheImage(theme.loadingImage(), context);
