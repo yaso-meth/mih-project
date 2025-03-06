78f5dede61fecc8a9e234c75e195771d4e171a21
Yasien Mac Mini <yasienmeth@gmail.com>
2025-03-06 10:33:31 +0200

    new About MIH Package start

---
+++ Frontend/lib/mih_packages/about_mih/about_mih.dart
@@ -0,0 +1,77 @@
+import 'package:Mzansi_Innovation_Hub/mih_components/mih_package/mih_app.dart';
+import 'package:Mzansi_Innovation_Hub/mih_components/mih_package/mih_app_action.dart';
+import 'package:Mzansi_Innovation_Hub/mih_components/mih_package/mih_app_tools.dart';
+import 'package:Mzansi_Innovation_Hub/mih_packages/about_mih/app_tools/mih_info.dart';
+import 'package:Mzansi_Innovation_Hub/mih_packages/about_mih/app_tools/mih_privacy_policy.dart';
+import 'package:Mzansi_Innovation_Hub/mih_packages/about_mih/app_tools/mih_terms_of_service.dart';
+import 'package:flutter/material.dart';
+
+class AboutMih extends StatefulWidget {
+  const AboutMih({super.key});
+
+  @override
+  State<AboutMih> createState() => _AboutMihState();
+}
+
+class _AboutMihState extends State<AboutMih> {
+  int _selcetedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MihApp(
+      appActionButton: getAction(),
+      appTools: getTools(),
+      appBody: getToolBody(),
+      selectedbodyIndex: _selcetedIndex,
+      onIndexChange: (newValue) {
+        setState(() {
+          _selcetedIndex = newValue;
+        });
+        print("Index: $_selcetedIndex");
+      },
+    );
+  }
+
+  MihAppAction getAction() {
+    return MihAppAction(
+      icon: const Icon(Icons.arrow_back),
+      iconSize: 35,
+      onTap: () {
+        Navigator.of(context).pop();
+      },
+    );
+  }
+
+  MihAppTools getTools() {
+    Map<Widget, void Function()?> temp = {};
+    temp[const Icon(Icons.info)] = () {
+      setState(() {
+        _selcetedIndex = 0;
+      });
+    };
+    temp[const Icon(Icons.policy)] = () {
+      setState(() {
+        _selcetedIndex = 1;
+      });
+    };
+    temp[const Icon(Icons.design_services)] = () {
+      setState(() {
+        _selcetedIndex = 2;
+      });
+    };
+
+    return MihAppTools(
+      tools: temp,
+      selcetedIndex: _selcetedIndex,
+    );
+  }
+
+  List<Widget> getToolBody() {
+    List<Widget> toolBodies = [
+      const MihInfo(),
+      const MihPrivacyPolicy(),
+      const MIHTermsOfService(),
+    ];
+    return toolBodies;
+  }
+}
