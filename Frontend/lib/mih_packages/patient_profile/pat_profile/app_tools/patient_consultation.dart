edbde7e29bc3e391e716fa2531e2f4f7129bc579
Yasien Mac Mini <yasienmeth@gmail.com>
2025-03-17 14:05:38 +0200

    new app tools

---
+++ Frontend/lib/mih_packages/patient_profile/pat_profile/app_tools/patient_consultation.dart
@@ -0,0 +1,361 @@
+import 'dart:convert';
+import 'package:Mzansi_Innovation_Hub/main.dart';
+import 'package:Mzansi_Innovation_Hub/mih_components/mih_inputs_and_buttons/mih_button.dart';
+import 'package:Mzansi_Innovation_Hub/mih_components/mih_inputs_and_buttons/mih_multiline_text_input.dart';
+import 'package:Mzansi_Innovation_Hub/mih_components/mih_inputs_and_buttons/mih_text_input.dart';
+import 'package:Mzansi_Innovation_Hub/mih_components/mih_layout/mih_single_child_scroll.dart';
+import 'package:Mzansi_Innovation_Hub/mih_components/mih_layout/mih_window.dart';
+import 'package:Mzansi_Innovation_Hub/mih_components/mih_package/mih-app_tool_body.dart';
+import 'package:Mzansi_Innovation_Hub/mih_components/mih_pop_up_messages/mih_error_message.dart';
+import 'package:Mzansi_Innovation_Hub/mih_components/mih_pop_up_messages/mih_loading_circle.dart';
+import 'package:Mzansi_Innovation_Hub/mih_components/mih_pop_up_messages/mih_success_message.dart';
+import 'package:Mzansi_Innovation_Hub/mih_env/env.dart';
+import 'package:Mzansi_Innovation_Hub/mih_objects/app_user.dart';
+import 'package:Mzansi_Innovation_Hub/mih_objects/business.dart';
+import 'package:Mzansi_Innovation_Hub/mih_objects/business_user.dart';
+import 'package:Mzansi_Innovation_Hub/mih_objects/notes.dart';
+import 'package:Mzansi_Innovation_Hub/mih_objects/patients.dart';
+import 'package:Mzansi_Innovation_Hub/mih_packages/patient_profile/pat_profile/list_builders/build_notes_list.dart';
+import 'package:flutter/material.dart';
+import 'package:supertokens_flutter/http.dart' as http;
+
+class PatientConsultation extends StatefulWidget {
+  final String patientAppId;
+  final Patient selectedPatient;
+  final AppUser signedInUser;
+  final Business? business;
+  final BusinessUser? businessUser;
+  final String type;
+  const PatientConsultation({
+    super.key,
+    required this.patientAppId,
+    required this.selectedPatient,
+    required this.signedInUser,
+    required this.business,
+    required this.businessUser,
+    required this.type,
+  });
+
+  @override
+  State<PatientConsultation> createState() => _PatientConsultationState();
+}
+
+class _PatientConsultationState extends State<PatientConsultation> {
+  late Future<List<Note>> futueNotes;
+  final titleController = TextEditingController();
+  final noteTextController = TextEditingController();
+  final officeController = TextEditingController();
+  final dateController = TextEditingController();
+  final doctorController = TextEditingController();
+  final ValueNotifier<int> _counter = ValueNotifier<int>(0);
+  String endpoint = "${AppEnviroment.baseApiUrl}/notes/patients/";
+
+  Future<List<Note>> fetchNotes(String endpoint) async {
+    final response = await http.get(Uri.parse(
+        "${AppEnviroment.baseApiUrl}/notes/patients/${widget.selectedPatient.app_id}"));
+    if (response.statusCode == 200) {
+      Iterable l = jsonDecode(response.body);
+      List<Note> notes =
+          List<Note>.from(l.map((model) => Note.fromJson(model)));
+      //print("Here notes");
+      return notes;
+    } else {
+      internetConnectionPopUp();
+      throw Exception('failed to load patients');
+    }
+  }
+
+  void addNotePopUp() {
+    DateTime now = new DateTime.now();
+    DateTime date = new DateTime(now.year, now.month, now.day);
+    var title = "";
+    print("Business User: ${widget.businessUser}");
+    if (widget.businessUser?.title == "Doctor") {
+      title = "Dr.";
+    }
+    setState(() {
+      officeController.text = widget.business!.Name;
+      doctorController.text =
+          "$title ${widget.signedInUser.fname} ${widget.signedInUser.lname}";
+      dateController.text = date.toString().substring(0, 10);
+    });
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => MIHWindow(
+        fullscreen: false,
+        windowTitle: "Add Note",
+        windowTools: const [],
+        onWindowTapClose: () {
+          Navigator.pop(context);
+          titleController.clear();
+          noteTextController.clear();
+        },
+        windowBody: [
+          MIHTextField(
+            controller: officeController,
+            hintText: "Office",
+            editable: false,
+            required: true,
+          ),
+          const SizedBox(height: 10.0),
+          MIHTextField(
+            controller: doctorController,
+            hintText: "Created By",
+            editable: false,
+            required: true,
+          ),
+          const SizedBox(height: 10.0),
+          MIHTextField(
+            controller: dateController,
+            hintText: "Created Date",
+            editable: false,
+            required: true,
+          ),
+          const SizedBox(height: 10.0),
+          MIHTextField(
+            controller: titleController,
+            hintText: "Note Title",
+            editable: true,
+            required: true,
+          ),
+          const SizedBox(height: 10.0),
+          SizedBox(
+            //width: 700,
+            height: 250,
+            child: MIHMLTextField(
+              controller: noteTextController,
+              hintText: "Note Details",
+              editable: true,
+              required: true,
+            ),
+          ),
+          SizedBox(
+            height: 15,
+            child: ValueListenableBuilder(
+              builder: (BuildContext context, int value, Widget? child) {
+                return Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      "$value",
+                      style: TextStyle(
+                        color: getNoteDetailLimitColor(),
+                      ),
+                    ),
+                    const SizedBox(width: 5),
+                    Text(
+                      "/512",
+                      style: TextStyle(
+                        color: getNoteDetailLimitColor(),
+                      ),
+                    ),
+                  ],
+                );
+              },
+              valueListenable: _counter,
+            ),
+          ),
+          const SizedBox(height: 15.0),
+          SizedBox(
+            width: 300,
+            height: 50,
+            child: MIHButton(
+              onTap: () {
+                if (isFieldsFilled()) {
+                  addPatientNoteAPICall();
+                  Navigator.pop(context);
+                } else {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return const MIHErrorMessage(errorType: "Input Error");
+                    },
+                  );
+                }
+              },
+              buttonText: "Add Note",
+              buttonColor:
+                  MzanziInnovationHub.of(context)!.theme.secondaryColor(),
+              textColor: MzanziInnovationHub.of(context)!.theme.primaryColor(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Future<void> addPatientNoteAPICall() async {
+    // String title = "";
+    // if (widget.businessUser!.title == "Doctor") {
+    //   title = "Dr.";
+    // }
+    var response = await http.post(
+      Uri.parse("${AppEnviroment.baseApiUrl}/notes/insert/"),
+      headers: <String, String>{
+        "Content-Type": "application/json; charset=UTF-8"
+      },
+      body: jsonEncode(<String, dynamic>{
+        "note_name": titleController.text,
+        "note_text": noteTextController.text,
+        "doc_office": officeController.text,
+        "doctor": doctorController.text,
+        "app_id": widget.selectedPatient.app_id,
+      }),
+    );
+    if (response.statusCode == 201) {
+      setState(() {
+        futueNotes = fetchNotes(endpoint + widget.patientAppId.toString());
+      });
+      // Navigator.of(context)
+      //     .pushNamed('/patient-manager', arguments: widget.userEmail);
+      String message =
+          "Your note has been successfully added to the patients medical record. You can now view it alongside their other important information.";
+      successPopUp(message);
+    } else {
+      internetConnectionPopUp();
+    }
+  }
+
+  bool isFieldsFilled() {
+    if (titleController.text.isEmpty ||
+        noteTextController.text.isEmpty ||
+        _counter.value >= 512) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  Color getNoteDetailLimitColor() {
+    if (_counter.value <= 512) {
+      return MzanziInnovationHub.of(context)!.theme.secondaryColor();
+    } else {
+      return MzanziInnovationHub.of(context)!.theme.errorColor();
+    }
+  }
+
+  List<Widget> setIcons() {
+    if (widget.type == "personal") {
+      return [
+        Text(
+          "Consultation Notes",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+              color: MzanziInnovationHub.of(context)!.theme.secondaryColor()),
+        ),
+      ];
+    } else {
+      return [
+        Text(
+          "Consultation Notes",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+              color: MzanziInnovationHub.of(context)!.theme.secondaryColor()),
+        ),
+        IconButton(
+          onPressed: () {
+            // addConsultationNotePopUp();
+            addNotePopUp();
+          },
+          icon: Icon(Icons.add,
+              color: MzanziInnovationHub.of(context)!.theme.secondaryColor()),
+        )
+      ];
+    }
+  }
+
+  void successPopUp(String message) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return MIHSuccessMessage(
+          successType: "Success",
+          successMessage: message,
+        );
+      },
+    );
+  }
+
+  void internetConnectionPopUp() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const MIHErrorMessage(errorType: "Internet Connection");
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    titleController.dispose();
+    noteTextController.dispose();
+    doctorController.dispose();
+    dateController.dispose();
+    officeController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    futueNotes = fetchNotes(endpoint + widget.patientAppId);
+    noteTextController.addListener(() {
+      setState(() {
+        _counter.value = noteTextController.text.characters.length;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MihAppToolBody(
+      borderOn: true,
+      bodyItem: getBody(),
+    );
+  }
+
+  Widget getBody() {
+    return MihSingleChildScroll(
+      child: FutureBuilder(
+        future: futueNotes,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: Mihloadingcircle(),
+            );
+          } else if (snapshot.hasData) {
+            final notesList = snapshot.data!;
+            return Column(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: setIcons(),
+              ),
+              Divider(
+                  color:
+                      MzanziInnovationHub.of(context)!.theme.secondaryColor()),
+              const SizedBox(height: 10),
+              BuildNotesList(
+                notes: notesList,
+                signedInUser: widget.signedInUser,
+                selectedPatient: widget.selectedPatient,
+                business: widget.business,
+                businessUser: widget.businessUser,
+                type: widget.type,
+              ),
+            ]);
+          } else {
+            return const Center(
+              child: Text("Error Loading Notes"),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ Frontend/lib/mih_packages/patient_profile/pat_profile/app_tools/patient_info.dart
@@ -0,0 +1,294 @@
+import 'package:Mzansi_Innovation_Hub/main.dart';
+import 'package:Mzansi_Innovation_Hub/mih_components/mih_inputs_and_buttons/mih_text_input.dart';
+import 'package:Mzansi_Innovation_Hub/mih_components/mih_layout/mih_single_child_scroll.dart';
+import 'package:Mzansi_Innovation_Hub/mih_components/mih_package/mih-app_tool_body.dart';
+import 'package:Mzansi_Innovation_Hub/mih_objects/app_user.dart';
+import 'package:Mzansi_Innovation_Hub/mih_objects/arguments.dart';
+import 'package:Mzansi_Innovation_Hub/mih_objects/patients.dart';
+import 'package:flutter/material.dart';
+
+class PatientInfo extends StatefulWidget {
+  final AppUser signedInUser;
+  final Patient selectedPatient;
+  final String type;
+  const PatientInfo({
+    super.key,
+    required this.signedInUser,
+    required this.selectedPatient,
+    required this.type,
+  });
+
+  @override
+  State<PatientInfo> createState() => _PatientInfoState();
+}
+
+class _PatientInfoState extends State<PatientInfo> {
+  final idController = TextEditingController();
+  final fnameController = TextEditingController();
+  final lnameController = TextEditingController();
+  final cellController = TextEditingController();
+  final emailController = TextEditingController();
+  final medNoController = TextEditingController();
+  final medNameController = TextEditingController();
+  final medSchemeController = TextEditingController();
+  final addressController = TextEditingController();
+  final medAidController = TextEditingController();
+  final medMainMemController = TextEditingController();
+  final medAidCodeController = TextEditingController();
+  double textFieldWidth = 400.0;
+  late String medAid;
+
+  Widget getPatientDetailsField() {
+    return Wrap(
+      spacing: 15,
+      runSpacing: 10,
+      children: [
+        SizedBox(
+          width: textFieldWidth,
+          child: MIHTextField(
+              controller: idController,
+              hintText: "ID No.",
+              editable: false,
+              required: false),
+        ),
+        SizedBox(
+          width: textFieldWidth,
+          child: MIHTextField(
+              controller: fnameController,
+              hintText: "Name",
+              editable: false,
+              required: false),
+        ),
+        SizedBox(
+          width: textFieldWidth,
+          child: MIHTextField(
+              controller: lnameController,
+              hintText: "Surname",
+              editable: false,
+              required: false),
+        ),
+        SizedBox(
+          width: textFieldWidth,
+          child: MIHTextField(
+              controller: cellController,
+              hintText: "Cell No.",
+              editable: false,
+              required: false),
+        ),
+        SizedBox(
+          width: textFieldWidth,
+          child: MIHTextField(
+              controller: emailController,
+              hintText: "Email",
+              editable: false,
+              required: false),
+        ),
+        SizedBox(
+          width: textFieldWidth,
+          child: MIHTextField(
+              controller: addressController,
+              hintText: "Address",
+              editable: false,
+              required: false),
+        ),
+      ],
+    );
+  }
+
+  Widget getMedAidDetailsFields() {
+    List<Widget> medAidDet = [];
+    medAidDet.add(SizedBox(
+      width: textFieldWidth,
+      child: MIHTextField(
+          controller: medAidController,
+          hintText: "Medical Aid",
+          editable: false,
+          required: false),
+    ));
+    bool req;
+    if (medAid == "Yes") {
+      req = true;
+    } else {
+      req = false;
+    }
+    medAidDet.addAll([
+      Visibility(
+        visible: req,
+        child: SizedBox(
+          width: textFieldWidth,
+          child: MIHTextField(
+              controller: medMainMemController,
+              hintText: "Main Member",
+              editable: false,
+              required: false),
+        ),
+      ),
+      //const SizedBox(height: 10.0),
+      Visibility(
+        visible: req,
+        child: SizedBox(
+          width: textFieldWidth,
+          child: MIHTextField(
+              controller: medNoController,
+              hintText: "No.",
+              editable: false,
+              required: false),
+        ),
+      ),
+      //const SizedBox(height: 10.0),
+      Visibility(
+        visible: req,
+        child: SizedBox(
+          width: textFieldWidth,
+          child: MIHTextField(
+              controller: medAidCodeController,
+              hintText: "Code",
+              editable: false,
+              required: false),
+        ),
+      ),
+      //const SizedBox(height: 10.0),
+      Visibility(
+        visible: req,
+        child: SizedBox(
+          width: textFieldWidth,
+          child: MIHTextField(
+              controller: medNameController,
+              hintText: "Name",
+              editable: false,
+              required: false),
+        ),
+      ),
+      //const SizedBox(height: 10.0),
+      Visibility(
+        visible: req,
+        child: SizedBox(
+          width: textFieldWidth,
+          child: MIHTextField(
+              controller: medSchemeController,
+              hintText: "Scheme",
+              editable: false,
+              required: false),
+        ),
+      ),
+      //),
+    ]);
+    return Wrap(
+      spacing: 10,
+      runSpacing: 10,
+      children: medAidDet,
+    );
+  }
+
+  @override
+  void dispose() {
+    idController.dispose();
+    fnameController.dispose();
+    lnameController.dispose();
+    cellController.dispose();
+    emailController.dispose();
+    medNameController.dispose();
+    medNoController.dispose();
+    medSchemeController.dispose();
+    addressController.dispose();
+    medAidController.dispose();
+    medMainMemController.dispose();
+    medAidCodeController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    setState(() {
+      idController.value = TextEditingValue(text: widget.selectedPatient.id_no);
+      fnameController.value =
+          TextEditingValue(text: widget.selectedPatient.first_name);
+      lnameController.value =
+          TextEditingValue(text: widget.selectedPatient.last_name);
+      cellController.value =
+          TextEditingValue(text: widget.selectedPatient.cell_no);
+      emailController.value =
+          TextEditingValue(text: widget.selectedPatient.email);
+      medNameController.value =
+          TextEditingValue(text: widget.selectedPatient.medical_aid_name);
+      medNoController.value =
+          TextEditingValue(text: widget.selectedPatient.medical_aid_no);
+      medSchemeController.value =
+          TextEditingValue(text: widget.selectedPatient.medical_aid_scheme);
+      addressController.value =
+          TextEditingValue(text: widget.selectedPatient.address);
+      medAidController.value =
+          TextEditingValue(text: widget.selectedPatient.medical_aid);
+      medMainMemController.value = TextEditingValue(
+          text: widget.selectedPatient.medical_aid_main_member);
+      medAidCodeController.value =
+          TextEditingValue(text: widget.selectedPatient.medical_aid_code);
+      medAid = widget.selectedPatient.medical_aid;
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MihAppToolBody(
+      borderOn: true,
+      bodyItem: getBody(),
+    );
+  }
+
+  Widget getBody() {
+    return MihSingleChildScroll(
+      child: Column(
+        children: [
+          Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              //crossAxisAlignment: ,
+              children: [
+                Text(
+                  "Personal Details",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color:
+                        MzanziInnovationHub.of(context)!.theme.secondaryColor(),
+                  ),
+                ),
+                Visibility(
+                  visible: widget.type == "personal",
+                  child: IconButton(
+                    icon: const Icon(Icons.edit),
+                    alignment: Alignment.topRight,
+                    color:
+                        MzanziInnovationHub.of(context)!.theme.secondaryColor(),
+                    onPressed: () {
+                      Navigator.of(context).pushNamed('/patient-profile/edit',
+                          arguments: PatientEditArguments(
+                              widget.signedInUser, widget.selectedPatient));
+                    },
+                  ),
+                )
+              ]),
+          Divider(
+              color: MzanziInnovationHub.of(context)!.theme.secondaryColor()),
+          const SizedBox(height: 10),
+          getPatientDetailsField(),
+          const SizedBox(height: 10),
+          Text(
+            "Medical Aid Details",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+              color: MzanziInnovationHub.of(context)!.theme.secondaryColor(),
+            ),
+          ),
+          Divider(
+              color: MzanziInnovationHub.of(context)!.theme.secondaryColor()),
+          const SizedBox(height: 10),
+          getMedAidDetailsFields(),
+        ],
+      ),
+    );
+  }
+}
