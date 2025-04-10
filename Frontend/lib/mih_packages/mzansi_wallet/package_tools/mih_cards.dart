c456b084ef38e6beba6be91732023c0e3768f91a
yaso <yasienmeth@gmail.com>
2025-04-10 12:47:23 +0200

    Add floating menu to Mzansi Wallet

--- Frontend/lib/mih_packages/mzansi_wallet/package_tools/mih_cards.dart
+++ Frontend/lib/mih_packages/mzansi_wallet/package_tools/mih_cards.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:mzansi_innovation_hub/main.dart';
 import 'package:mzansi_innovation_hub/mih_apis/mih_mzansi_wallet_apis.dart';
 import 'package:mzansi_innovation_hub/mih_components/mih_inputs_and_buttons/mih_button.dart';
@@ -7,6 +8,7 @@ import 'package:mzansi_innovation_hub/mih_components/mih_inputs_and_buttons/mih_
 import 'package:mzansi_innovation_hub/mih_components/mih_layout/mih_single_child_scroll.dart';
 import 'package:mzansi_innovation_hub/mih_components/mih_layout/mih_window.dart';
 import 'package:mzansi_innovation_hub/mih_components/mih_package_components/mih-app_tool_body.dart';
+import 'package:mzansi_innovation_hub/mih_components/mih_package_components/mih_floating_menu.dart';
 import 'package:mzansi_innovation_hub/mih_components/mih_pop_up_messages/mih_error_message.dart';
 import 'package:mzansi_innovation_hub/mih_components/mih_pop_up_messages/mih_loading_circle.dart';
 import 'package:mzansi_innovation_hub/mih_objects/app_user.dart';
@@ -316,24 +318,28 @@ class _MihCardsState extends State<MihCards> {
           ),
         ),
         Positioned(
-            right: 0,
-            bottom: 0,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                color: MzanziInnovationHub.of(context)!.theme.secondaryColor(),
-              ),
-              child: IconButton(
+          right: 0,
+          bottom: 0,
+          child: MihFloatingMenu(children: [
+            SpeedDialChild(
+              child: Icon(
+                Icons.add,
                 color: MzanziInnovationHub.of(context)!.theme.primaryColor(),
-                onPressed: () {
-                  addCardWindow(context);
-                },
-                icon: const Icon(
-                  Icons.add_card,
-                  size: 50,
-                ),
               ),
-            ))
+              label: "Add",
+              labelBackgroundColor:
+                  MzanziInnovationHub.of(context)!.theme.secondaryColor(),
+              labelStyle: TextStyle(
+                color: MzanziInnovationHub.of(context)!.theme.primaryColor(),
+              ),
+              backgroundColor:
+                  MzanziInnovationHub.of(context)!.theme.secondaryColor(),
+              onTap: () {
+                addCardWindow(context);
+              },
+            )
+          ]),
+        )
       ],
     );
   }
