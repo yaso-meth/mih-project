f1cfbe7713de03911e795200fca3d45391596a5f
Yasien Mac Mini <yasienmeth@gmail.com>
2025-04-15 13:32:34 +0200

    add error message if auth is not set up already

--- Frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import app_settings
 import device_info_plus
 import file_picker
 import flutter_tts
@@ -20,6 +21,7 @@ import url_launcher_macos
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppSettingsPlugin.register(with: registry.registrar(forPlugin: "AppSettingsPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
