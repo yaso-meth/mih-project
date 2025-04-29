aad72a26bfc45ebf8ee82bfb6cec9bb46b8c7518
Yasien Mac Mini <yasienmeth@gmail.com>
2025-04-29 13:44:19 +0200

    get prof urls

--- backend/Minio_Storage/minioConnection.py
+++ backend/Minio_Storage/minioConnection.py
@@ -1,7 +1,7 @@
 from minio import Minio
 
 def minioConnect(env):
-    # if(env == "Dev"):
+    if(env == "Dev"):
         return Minio(
         "minio:9000",
         # "minio.mzansi-innovation-hub.co.za",
@@ -9,11 +9,11 @@ def minioConnect(env):
         secret_key="nEED72ZlKYgDqH9Iy46fVGGT9TfabGWO",
         secure=False
         )
-    # else:
-    #     return Minio(
-    #     #"minio:9000",
-    #     "minio.mzansi-innovation-hub.co.za",
-    #     access_key="0RcgutfvcDq28lz7",
-    #     secret_key="nEED72ZlKYgDqH9Iy46fVGGT9TfabGWO",
-    #     secure=True
-    #     )
+    else:
+        return Minio(
+        #"minio:9000",
+        "minio.mzansi-innovation-hub.co.za",
+        access_key="0RcgutfvcDq28lz7",
+        secret_key="nEED72ZlKYgDqH9Iy46fVGGT9TfabGWO",
+        secure=True
+        )
