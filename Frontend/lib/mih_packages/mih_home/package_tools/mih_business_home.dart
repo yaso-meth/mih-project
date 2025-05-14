ced135301018925b4f92883ac715118bfdd5c537
Yasien Mac Mini <yasienmeth@gmail.com>
2025-05-14 14:43:39 +0200

    reomve banner

--- Frontend/lib/mih_packages/mih_home/package_tools/mih_business_home.dart
+++ Frontend/lib/mih_packages/mih_home/package_tools/mih_business_home.dart
@@ -199,41 +199,41 @@ class _MihBusinessHomeState extends State<MihBusinessHome>
     return MihSingleChildScroll(
       child: Column(
         children: [
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: MzanziInnovationHub.of(context)!.theme.errorColor(),
-            ),
-            height: 40,
-            child: Row(
-              children: [
-                Icon(
-                  Icons.warning_amber_rounded,
-                  color: MzanziInnovationHub.of(context)!.theme.primaryColor(),
-                  size: 40,
-                ),
-                const SizedBox(
-                  width: 5,
-                ),
-                Expanded(
-                  child: SingleChildScrollView(
-                    controller: _scrollController,
-                    scrollDirection: Axis.horizontal,
-                    child: Text(
-                      maintenanceMsg,
-                      style: TextStyle(
-                        color: MzanziInnovationHub.of(context)!
-                            .theme
-                            .primaryColor(),
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
+          // Container(
+          //   decoration: BoxDecoration(
+          //     borderRadius: BorderRadius.circular(10),
+          //     color: MzanziInnovationHub.of(context)!.theme.errorColor(),
+          //   ),
+          //   height: 40,
+          //   child: Row(
+          //     children: [
+          //       Icon(
+          //         Icons.warning_amber_rounded,
+          //         color: MzanziInnovationHub.of(context)!.theme.primaryColor(),
+          //         size: 40,
+          //       ),
+          //       const SizedBox(
+          //         width: 5,
+          //       ),
+          //       Expanded(
+          //         child: SingleChildScrollView(
+          //           controller: _scrollController,
+          //           scrollDirection: Axis.horizontal,
+          //           child: Text(
+          //             maintenanceMsg,
+          //             style: TextStyle(
+          //               color: MzanziInnovationHub.of(context)!
+          //                   .theme
+          //                   .primaryColor(),
+          //               fontSize: 20,
+          //               fontWeight: FontWeight.bold,
+          //             ),
+          //           ),
+          //         ),
+          //       ),
+          //     ],
+          //   ),
+          // ),
           const SizedBox(
             height: 5,
           ),
