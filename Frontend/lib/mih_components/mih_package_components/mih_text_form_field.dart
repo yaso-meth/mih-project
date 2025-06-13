bea2a6df3dc04a5928a454a8985975577ad704ea
Yasien Mac Mini <yasienmeth@gmail.com>
2025-06-13 11:39:57 +0200

    enable on text Input fields

--- Frontend/lib/mih_components/mih_package_components/mih_text_form_field.dart
+++ Frontend/lib/mih_components/mih_package_components/mih_text_form_field.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:mzansi_innovation_hub/main.dart';
@@ -152,6 +153,11 @@ class _MihTextFormFieldState extends State<MihTextFormField> {
                             controller: widget.controller,
                             cursorColor: widget.inputColor,
                             autofillHints: widget.autofillHints,
+                            spellCheckConfiguration: (kIsWeb ||
+                                    widget.passwordMode == true ||
+                                    widget.numberMode == true)
+                                ? null
+                                : SpellCheckConfiguration(),
                             textAlign:
                                 widget.textIputAlignment ?? TextAlign.start,
                             textAlignVertical: widget.multiLineInput == true
