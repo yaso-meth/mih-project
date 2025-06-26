6bb431b8943ba7872e525e47f9fbd66ac2dc4003
Yasien Mac Mini <yasienmeth@gmail.com>
2025-06-26 16:36:49 +0200

    add url strat

--- Frontend/lib/main_prod.dart
+++ Frontend/lib/main_prod.dart
@@ -1,31 +1,13 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:mzansi_innovation_hub/main.dart';
 import 'package:pwa_install/pwa_install.dart';
 import 'mih_config/mih_env.dart';
-import '../../main.dart';
 import 'package:supertokens_flutter/supertokens.dart';
-// import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
-// void main() async {
-//   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-//   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-//   AppEnviroment.setupEnv(Enviroment.prod);
-//   SuperTokens.init(
-//     apiDomain: AppEnviroment.baseApiUrl,
-//     apiBasePath: "/auth",
-//   );
-//   // setUrlStrategy(PathUrlStrategy());
-//   if (!kIsWeb) {
-//     MobileAds.instance.initialize();
-//   }
-//   FlutterNativeSplash.remove();
-//   PWAInstall().setup(installCallback: () {
-//     debugPrint('APP INSTALLED!');
-//   });
-//   runApp(const MzanziInnovationHub());
-// }
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
@@ -36,6 +18,8 @@ void main() async {
   );
   if (!kIsWeb) {
     MobileAds.instance.initialize();
+  } else {
+    usePathUrlStrategy();
   }
   PWAInstall().setup(installCallback: () {
     debugPrint('APP INSTALLED!');
