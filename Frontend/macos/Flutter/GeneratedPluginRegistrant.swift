52b71cef61ccb18f9fec1bfb7f0f1a24d3411659
Yasien Mac Mini <yasienmeth@gmail.com>
2025-09-22 09:13:39 +0200

    update android build

--- Frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,6 +12,7 @@ import flutter_tts
 import geolocator_apple
 import local_auth_darwin
 import mobile_scanner
+import package_info_plus
 import path_provider_foundation
 import printing
 import screen_brightness_macos
@@ -30,6 +31,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLALocalAuthPlugin.register(with: registry.registrar(forPlugin: "FLALocalAuthPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   ScreenBrightnessMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenBrightnessMacosPlugin"))
