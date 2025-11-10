a6f6cdaace63c18cd77d41eab952c8ee2632dd96
Yasien Mac Mini <yasienmeth@gmail.com>
2025-11-10 14:19:40 +0200

    NEW: MIH Stands with Women For Change SA

--- Frontend/lib/mih_config/mih_theme.dart
+++ Frontend/lib/mih_config/mih_theme.dart
@@ -14,7 +14,7 @@ class MihTheme {
   late String loadingAssetText;
   late TargetPlatform platform;
   bool kIsWeb = const bool.fromEnvironment('dart.library.js_util');
-  String latestVersion = "1.2.2";
+  String latestVersion = "1.2.3";
   // Options:-
   // f3f9d2 = Cream
   // f0f0c9 = cream2
