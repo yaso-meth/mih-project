c45c93327767ee4b45c2e25b8b71d71aa741770c
Yasien Mac Mini <yasienmeth@gmail.com>
2025-11-27 13:16:30 +0200

    Update version and build

--- Frontend/lib/mih_config/mih_theme.dart
+++ Frontend/lib/mih_config/mih_theme.dart
@@ -14,7 +14,7 @@ class MihTheme {
   late String loadingAssetText;
   late TargetPlatform platform;
   bool kIsWeb = const bool.fromEnvironment('dart.library.js_util');
-  String latestVersion = "1.2.4";
+  String latestVersion = "1.2.5";
   // Options:-
   // f3f9d2 = Cream
   // f0f0c9 = cream2
