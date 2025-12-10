e3ac1be71ce3f2ed519834d22b69391317ba0d8c
Yasien Mac Mini <yasienmeth@gmail.com>
2025-12-10 12:52:31 +0200

    BUG: Business Profile Vew pt2

--- Frontend/lib/mih_config/mih_go_router.dart
+++ Frontend/lib/mih_config/mih_go_router.dart
@@ -198,6 +198,7 @@ class MihGoRouter {
           }
           return MzansiBusinessProfileView(
             businessId: businessId,
+            fromMzansiDirectory: businessId == null,
           );
         },
       ),
