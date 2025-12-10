777043e2ca0b4692facaa0f683d384825562dab5
Yasien Mac Mini <yasienmeth@gmail.com>
2025-12-10 10:18:57 +0200

    NEW: Config firebase Cloud Messaging for Notifications

--- Frontend/lib/main_dev.dart
+++ Frontend/lib/main_dev.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
@@ -5,6 +6,7 @@ import 'package:flutter_web_plugins/url_strategy.dart'
     if (dart.library.html) 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:mzansi_innovation_hub/firebase_options.dart';
 import 'package:mzansi_innovation_hub/main.dart';
 import 'package:mzansi_innovation_hub/mih_config/mih_go_router.dart';
 import 'package:pwa_install/pwa_install.dart';
@@ -18,6 +20,9 @@ void main() async {
   SuperTokens.init(
     apiDomain: AppEnviroment.baseApiUrl,
     apiBasePath: "/auth",
+  );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   if (!kIsWeb) {
     const List<String> testDeviceIds = ['733d4c68-9b54-453a-9622-2df407310f40'];
--- Frontend/lib/main_prod.dart
+++ Frontend/lib/main_prod.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
@@ -5,6 +6,7 @@ import 'package:flutter_web_plugins/url_strategy.dart'
     if (dart.library.html) 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:mzansi_innovation_hub/firebase_options.dart';
 import 'package:mzansi_innovation_hub/main.dart';
 import 'package:mzansi_innovation_hub/mih_config/mih_go_router.dart';
 import 'package:pwa_install/pwa_install.dart';
@@ -19,6 +21,9 @@ void main() async {
     apiDomain: AppEnviroment.baseApiUrl,
     apiBasePath: "/auth",
   );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   if (!kIsWeb) {
     MobileAds.instance.initialize();
   } else {
@@ -29,5 +34,7 @@ void main() async {
   });
   final GoRouter appRouter = MihGoRouter().mihRouter;
   FlutterNativeSplash.remove();
-  runApp(MzansiInnovationHub(router: appRouter,));
+  runApp(MzansiInnovationHub(
+    router: appRouter,
+  ));
 }
